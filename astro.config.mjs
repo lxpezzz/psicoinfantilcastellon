10e67ced8cd48e0b29156d7abd725787131e3402
Héctor López Díaz <hector964@protonmail.com>
2026-09-25 10:54:59 +0200

    Actualizado el robots para que no indexe

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,6 +10,14 @@ export default defineConfig({
   redirects: {
     '/cookies': '/politica-de-cookies',
     '/politica-de-privacidad': '/privacidad',
+    '/asesoramiento-a-padres': '/services/asesoramiento-a-padres',
+    '/evaluacion-y-diagnostico': '/services/evaluacion-y-diagnostico',
+    '/informes-psicopedagogicos': '/services/informes-psicopedagogicos',
+    '/mejora-del-rendimiento-escolar': '/services/mejora-del-rendimiento-escolar',
+    '/orientacion-adolescentes': '/services/orientacion-adolescentes',
+    '/tratamiento-altas-capacidades': '/services/tratamiento-altas-capacidades',
+    '/tratamiento-dislexia': '/services/tratamiento-dislexia',
+    '/tratamiento-tdah': '/services/tratamiento-tdah',
   },
   vite: {
     plugins: [tailwindcss()],
